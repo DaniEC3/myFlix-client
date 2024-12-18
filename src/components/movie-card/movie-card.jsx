44724197398fccf6173bae91b86bedefcb505f17
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2024-12-18 18:48:08 -0500

    PropTypes added

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +9,20 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.name}
     </div>
   );
+};
+
+
+// set the static PropTypes property on MovieCard
+//  to an object that contains special values provided
+//  as utilities by prop-types.
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    director: PropTypes.string,
+    year_released: PropTypes.string,
+    imagePath: PropTypes.string,
+    description: PropTypes.string,
+    genre: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
