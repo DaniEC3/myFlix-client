69f3da50017edf9f18d72963b1d62cdd3be5ffb8
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2024-12-18 19:08:26 -0500

    Reapply "PropTypes added"
    This reverts commit 58e428b9aac70d497625706dba4d42a647f802ec.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,67 +1,30 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      name: "The Godfather",
-      description: "The patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
-      genre: "Drama",
-      director: "Francis Ford Coppola",
-      year_released: 1972,
-      imagePath: "images/the-godfather.jpg"
-    },
-    {
-      id: 2,
-      name: "Pulp Fiction",
-      description: "The lives of two hitmen, a boxer, a gangster, and his wife intertwine in four tales of crime and redemption.",
-      genre: "Crime",
-      director: "Quentin Tarantino",
-      year_released: 1994,
-      imagePath: "images/pulp-fiction.jpg"
-    },
-    {
-      id: 3,
-      name: "Spirited Away",
-      description: "A young girl becomes trapped in a mysterious world and must navigate a world of spirits to save her parents.",
-      genre: "Animation",
-      director: "Hayao Miyazaki",
-      year_released: 2001,
-      imagePath: "images/spirited-away.jpg"
-    },
-    {
-      id: 4,
-      name: "Get Out",
-      description: "A young African-American man uncovers disturbing secrets when he visits his white girlfriend's family estate.",
-      genre: "Horror",
-      director: "Jordan Peele",
-      year_released: 2017,
-      imagePath: "images/get-out.jpg"
-    },
-    {
-      id: 5,
-      name: "The Avengers",
-      description: "Earth's mightiest heroes must come together to stop Loki and his alien army from enslaving humanity.",
-      genre: "Action",
-      director: "Joss Whedon",
-      year_released: 2012,
-      imagePath: "images/the-avengers.jpg"
-    },
-    {
-      id: 6,
-      name: "La La Land",
-      description: "A jazz musician and an aspiring actress fall in love while chasing their dreams in Los Angeles.",
-      genre: "Romance",
-      director: "Damien Chazelle",
-      year_released: 2016,
-      imagePath: "images/la-la-land.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://openlibrary.org/search.json?q=star+wars")
+      .then((response) => response.json())
+      .then((data) => {
+        const booksFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image:
+`https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+            author: doc.author_name?.[0]
+          };
+        });
+
+        setMovies(booksFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
