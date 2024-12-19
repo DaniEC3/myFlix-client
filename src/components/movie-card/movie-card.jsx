58e428b9aac70d497625706dba4d42a647f802ec
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2024-12-18 19:05:40 -0500

    Revert "PropTypes added"
    This reverts commit 44724197398fccf6173bae91b86bedefcb505f17.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -9,20 +8,4 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.name}
     </div>
   );
-};
-
-
-// set the static PropTypes property on MovieCard
-//  to an object that contains special values provided
-//  as utilities by prop-types.
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    director: PropTypes.string,
-    year_released: PropTypes.string,
-    imagePath: PropTypes.string,
-    description: PropTypes.string,
-    genre: PropTypes.string
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
